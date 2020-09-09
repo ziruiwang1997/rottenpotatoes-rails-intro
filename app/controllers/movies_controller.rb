cc5fc73cb83aa0dc59d84ed914e8c7ff4166fef1
EC2 Default User <ec2-user@ip-172-31-21-222.us-east-2.compute.internal>
2020-09-09 06:46:56 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,15 +11,33 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Movie.all_ratings 
-    (params[:sort]) ? (@sort = params[:sort]) : (@sort = session[:sort])
-    (params[:ratings]) ? (@ratings = params[:ratings]) : (@ratings= session[:ratings])
-    @ratings = Hash[@all_ratings.map{ |x| [x, x] } ] || @ratings#array map to hash
-    session[:sort], session[:ratings] = @sort, @ratings 
+    
+    # @all_ratings = Movie.all_ratings 
+    # (params[:sort]) ? (@sort = params[:sort]) : (@sort = session[:sort])
+    # (params[:ratings]) ? (@ratings = params[:ratings]) : (@ratings= session[:ratings])
+    # @ratings = Hash[@all_ratings.map{ |x| [x, x] } ] || @ratings#array map to hash
+    # session[:sort], session[:ratings] = @sort, @ratings 
+    # if params[:ratings].nil? 
+    #   redirect_to :ratings => @ratings, :sort => @sort #when params is 
+    # end
+    # @movies = Movie.where(:rating => @ratings.keys).order @sort
+    
+    
+    @all_ratings = Movie.all_ratings # controller sets this variable by consulting the Model
+    @sort = params[:sort] || session[:sort] 
+    @ratings = params[:ratings] || session[:ratings]
+    @ratings ||= Hash[@all_ratings.collect { |item| [item, 1] } ]#array map to hash
+    session[:sort] = @sort
+    session[:ratings] = @ratings 
+    
+    @movies = Movie.with_ratings(@ratings.keys).order @sort # a class-level method in the model
+    #go in the model rather than exposing details of the schema to the controller
+    
     if params[:ratings].nil? 
       redirect_to :ratings => @ratings, :sort => @sort #when params is 
     end
-    @movies = Movie.where(:rating => @ratings.keys).order @sort
+    
+    
   end
 
   def new
