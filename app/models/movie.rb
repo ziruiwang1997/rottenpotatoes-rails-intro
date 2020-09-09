56217f8784edcf922c442eb5d1104c8a53c65605
EC2 Default User <ec2-user@ip-172-31-21-222.us-east-2.compute.internal>
2020-09-09 06:44:39 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
     def self.all_ratings #the possible values of movie ratings are really the responsibility of the Movie model
-        %w(G PG PG-13 NC-17 R)
+        Movie.uniq.pluck(:rating).sort
     end
 end
